79b94e55be2f3628f13f6620ae1d4d1a8ea051ab
ingun <43471475+parkingun@users.noreply.github.com>
2024-10-30 16:55:14 +0900

    Add say_hello

--- calculator.py
+++ calculator.py
@@ -3,5 +3,10 @@ def add(a, b):
   return a+b
 def subtract(a, b):
   return a-b
-def multiply (a, b):
+def multiply(a, b):
   return a*b
+def devide_new(a, b):
+  return a/b
+
+def say_hello():
+  print("hello)
