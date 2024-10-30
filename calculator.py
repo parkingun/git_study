4c56e32ffdffe4f70073b87e8764d9e419914105
paku <engi9364@naver.com>
2024-10-30 17:49:10 +0900

    Revert "Add square function"
    This reverts commit 50be4815217ffd856dd9faab2fd9ea6fb2aaa196.

--- calculator.py
+++ calculator.py
@@ -1,15 +1,9 @@
 # 기본 계산기
 def add(a, b):
   return a+b
-
 def subtract(a, b):
   return a-b
-
 def multiply(a, b):
   return a*b
-
 def devide_new(a, b):
   return a/b
-
-def square(a):
-  return a*a
